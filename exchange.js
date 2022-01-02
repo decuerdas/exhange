7a3443245d8d1323a1d72b6629593f27905e39c6
decuerdas <decuerdas@gmail.com>
2022-01-02 15:31:56 -0500

    Delete exchange.js

--- exchange.js
+++
@@ -1,8 +0,0 @@
-function exchange(){
-		try{
-		var value = parseFloat(document.getElementById('valor').value) || 0;
-		var dato = parseFloat(document.getElementById('dato').value) || 0;
-		var result;
-		document.getElementById("result").value = valor * dato
-			} catch (e) {}
-	}
